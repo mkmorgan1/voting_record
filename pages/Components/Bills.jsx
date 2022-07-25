0ee09375cd5a31406ed8d618f4cf6e6ec50c58bb
Matthew <m_kmorgan@hotmail.com>
2022-07-24 21:08:25 -0600

    FEAT: added key value tags to bills

--- pages/Components/Bills.jsx
+++ pages/Components/Bills.jsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from "react";
 import DropDown from "./DropDown.jsx";
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default function Bills({ bills }) {
   return bills.map((bill) => {
@@ -24,6 +29,7 @@ function Bill({ bill }) {
 function BillDropDown({ bill }) {
   return (
     <>
+      <BillStatus bill={bill} />
       <BillSummary bill={bill} />
       <BillLinks bill={bill} />
     </>
@@ -35,19 +41,47 @@ function BillSummary({ bill }) {
     <DropDown
       id={bill.bill_id + "Summary"}
       title={<h6>Summary:</h6>}
-      body={bill.summary ? bill.summary : "MISSING INFO"}
+      body={bill.summary ? bill.summary : "Missing summary information"}
     />
   );
 }
 
-function BillLinks({ bill }) {
-  ``;
-  const makeLink = (link, link_name) => {
-    <a href={link}>{link_name}</a>;
+function BillStatus({ bill }) {
+  const getPassed = (date) => {
+    if (date) {
+      return `Passed ${date}`;
+    } else if (bill.active) {
+      return "Not passed yet";
+    } else {
+      return "Was not passed";
+    }
   };
-  const congressUrl = makeLink(bill.congressdotgov_url, "congress.gov");
-  const govtrackUrl = makeLink(bill.govtrack_url, "govtrack.us");
-
+  return (
+    <>
+      <DropDown
+        id={bill.bill_id + "status"}
+        title={<h6>Status:</h6>}
+        body={
+          <>
+            <TitleValueTag
+              title={"Active"}
+              value={capitalizeFirstLetter(String(bill.active))}
+            />
+            <TitleValueTag
+              title={"House"}
+              value={getPassed(bill.house_passage)}
+            />
+            <TitleValueTag
+              title={"Senate"}
+              value={getPassed(bill.senate_passage)}
+            />
+          </>
+        }
+      />
+    </>
+  );
+}
+function BillLinks({ bill }) {
   return (
     <>
       <DropDown
@@ -55,11 +89,32 @@ function BillLinks({ bill }) {
         title={<h6>More info:</h6>}
         body={
           <>
-            {bill.congressdotgov_url ? congressUrl : ""}
-            {bill.govtrack_url ? govtrackUrl : ""}
+            <MakeLink name={"congress.gov"} link={bill.congressdotgov_url} />
+            <MakeLink name={"govtrack.us"} link={bill.govtrack_url} />
           </>
         }
       />
+    </>
+  );
+}
+
+function MakeLink({ name, link }) {
+  return link ? (
+    <a href={link} target="_blank">
+      {name}
+    </a>
+  ) : (
+    <></>
+  );
+}
+
+function TitleValueTag({ title, value }) {
+  return (
+    <>
+      <h6>
+        <b>{title}: </b>
+        {value}
+      </h6>
     </>
   );
 }
